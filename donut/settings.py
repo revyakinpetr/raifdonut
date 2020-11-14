77022323996a50eb483bc868b034097903a99054
Petr <revyakinpa@ya.ru>
2020-11-14 17:34:56 +0300

    added cors

--- donut/settings.py
+++ donut/settings.py
@@ -125,3 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_ALLOW_ALL = True
+
+CORS_ORIGIN_WHITELIST = (
+    'localhost'
+)
